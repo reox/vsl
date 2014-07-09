0a305722eed5b40b97f14d7c0e776d7a65390b4d
Sebastian Bachmann <me@free-minds.net>
2014-07-09 14:27:33 +0200

    adding the first working template output

--- scraper/get_conf.py
+++ scraper/get_conf.py
@@ -3,6 +3,7 @@
 import urllib.request
 from html.parser import HTMLParser
 from collections import defaultdict
+import re
 
 
 class MyHTMLParser(HTMLParser):
@@ -55,7 +56,20 @@ class MyHTMLParser(HTMLParser):
             self.found_content = 0
         elif self.found_content == 3 and self.inheading:
         # we found the title
-            self.title = data.rsplit('(')[-1][:-1]
+            if 'joint session of 10 meetings' in data:
+                if len(data.split('(')) > 2:
+                    # there should be another title in front
+                    self.title = data.rsplit('(')[-2][:-3].replace('  and ', ', ')
+                else:
+                    # otherwise take the front stuff
+                    self.title = data.split(':', 1)[1].rsplit('(', 1)[0][1:-1]
+                    if ':' in self.title:
+                        # long Olympic Games title
+                        self.title = self.title.split(':', 1)[0]
+
+            else:
+                self.title = data.rsplit('(')[-1][:-1].replace('  and ', ', ')
+
         elif self.found_content == 4 and self.inroom:
         # we found the room
             room = data
@@ -67,7 +81,6 @@ class MyHTMLParser(HTMLParser):
         return self.events
 
 
-
 def main():
     url = "http://www.easychair.org/smart-program/VSL2014/program.html"
     response = urllib.request.urlopen(url)
@@ -76,19 +89,52 @@ def main():
     parser = MyHTMLParser()
     parser.feed(html.decode('UTF-8', 'replace'))
 
-    for k, v in parser.get_result().items():
-        print(k)
-        for x,y in v:
-            print('\t', x, '---', y)
+    remove_title = ['VSL', 'INFINITY']
+    remove_place = ['Naturhistorisches Museum', 'Schönbrunn', 'FH, 2nd floor']
+    replace_place = {'FH, Dissertantenraum E104': 'FH, Dissertantenraum'}
 
-    # Need to clean the dataset.
-    # Events that are called VSL go out.
-    # every room begins with the building code
-    # we have some events with 'and'
-    # some special cases (selection...)
-    # joint session of 10 meetings
-    #  EI, EI 7 + EI 9, EI 10 + FH, Hörsaal 1
-    #   ARW-DT, VERIFY  and WING
+    date_matcher = re.compile("[A-Za-z, ]+([0-9]{1,2})[a-z]+")
+    all_events = {}
+    for k, v in parser.get_result().items():
+        date = date_matcher.match(k).group(1) 
+        events = defaultdict(list)
+        for x, y in v:
+            if x not in remove_title and y not in remove_place:
+                if y in replace_place:
+                    y = replace_place[y]
+
+                # now we need to replace the area and room:
+                if ',' in y:
+                    building, room = y.split(',')
+                    room = room[1:]
+                else:
+                    building = None
+                    room = y
+                events[building].append((x, room))
+        if len(events) > 0:
+            all_events[date] = events
+
+    # all_events contains now a {date-> {building -> [events (name, room)]}}
+
+    room_lookup = {}
+    # room lookup is used to look up rooms
+    with open("rooms.csv", "r") as f:
+        for line in f.readlines():
+            building, room, area, floor = line.replace('\n', '').split(',')
+            room_lookup[(building, room)] = (area, floor)
+
+    # now generate the signs...
+    # generate event signs for FH: 
+    event_list_raw = open("templates/fh_events.tex.tmpl", "r").read()
+    for date, events in all_events.items():
+        event_list = event_list_raw
+        event_list = event_list.replace("$$date$$", date)
+        table = "\n".join("%s & %s & %s \\\\" % (event, room_lookup[('FH', room)][0],
+            room_lookup[('FH', room)][1]) for event, room in sorted(events['FH']))
+        event_list = event_list.replace("$$events$$", table)
+        
+        with open("out/fh_event_%s.tex" % date, "w+") as f:
+            f.write(event_list)
 
 
 if __name__ == "__main__":
